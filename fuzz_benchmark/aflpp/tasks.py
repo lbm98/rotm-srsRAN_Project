3966a0671b57a0767779f13ef0bd3fd949379929
lbm <lars.moons@student.uantwerpen.be>
2023-11-21 16:42:30 +0100

    more

--- fuzz_benchmark/aflpp/tasks.py
+++ fuzz_benchmark/aflpp/tasks.py
@@ -1,12 +1,16 @@
 import os
+import sys
 import json
 import pathlib
 
-from invoke import task, Context
+from invoke import Context, task
 from dataclasses import dataclass
 
 THIS_SCRIPT_DIR = pathlib.Path(__file__).parent.resolve()
-CMAKE_SOURCE_DIR = THIS_SCRIPT_DIR.parent.parent.resolve()
+
+sys.path.append(str(THIS_SCRIPT_DIR.parent))
+
+import targets.srsran
 
 
 @dataclass
@@ -22,29 +26,28 @@ class AFL:
 
 @dataclass
 class FUZZ:
-    BUILD_DIR = THIS_SCRIPT_DIR / 'fuzz_build'
-    LIB_DIR = THIS_SCRIPT_DIR / 'fuzz_lib'
-    BIN_DIR = THIS_SCRIPT_DIR / 'fuzz_bin'
-    BIN = BIN_DIR / 'harness'
-    INFO_DIR = THIS_SCRIPT_DIR / 'fuzz_info'
+    DIR = THIS_SCRIPT_DIR / 'fuzz'
+    BUILD_DIR = DIR / 'build'
+    LIB_DIR = DIR / 'lib'
+    BIN_DIR = DIR / 'bin'
+    BIN = BIN_DIR / 'fuzzer'
+    INFO_DIR = DIR / 'info'
 
 
 @dataclass
 class COVERAGE:
-    BUILD_DIR = THIS_SCRIPT_DIR / 'coverage_build'
-    LIB_DIR = THIS_SCRIPT_DIR / 'coverage_lib'
-    BIN_DIR = THIS_SCRIPT_DIR / 'coverage_bin'
-    BIN = BIN_DIR / 'harness'
-    INFO_DIR = THIS_SCRIPT_DIR / 'coverage_info'
+    DIR = THIS_SCRIPT_DIR / 'coverage'
+    BUILD_DIR = DIR / 'build'
+    LIB_DIR = DIR / 'lib'
+    BIN_DIR = DIR / 'bin'
+    BIN = BIN_DIR / 'fuzzer'
+    INFO_DIR = DIR / 'info'
 
     LLVM_PROFRAW = INFO_DIR / 'fuzzer.profraw'
     LLVM_PROFDATA = INFO_DIR / 'fuzzer.profdata'
     EXPORT_JSON = INFO_DIR / 'export.json'
     TOTALS_JSON = INFO_DIR / 'totals.json'
 
-
-STANDALONE_FUZZ_TARGET_MAIN = THIS_SCRIPT_DIR / 'StandaloneFuzzTargetMain'
-CORPUS_DIR = THIS_SCRIPT_DIR / 'output/default/queue'
 
 os.environ['LLVM_CONFIG'] = 'llvm-config-14'
 
@@ -67,98 +70,95 @@ def build_aflpp(ctx: Context):
 
 @task
 def build_lib(ctx: Context):
-    if FUZZ.BUILD_DIR.exists() or FUZZ.LIB_DIR.exists():
-        print('srsRAN libraries already build...')
-        return
-
-    ctx.run(f'mkdir {FUZZ.BUILD_DIR}')
-
-    with ctx.cd(FUZZ.BUILD_DIR):
-        ctx.run(
-            f'cmake '
-            f'-G Ninja '
-            f'-DCMAKE_C_COMPILER={AFL.CLANG_FAST} '
-            f'-DCMAKE_CXX_COMPILER={AFL.CLANG_FASTPP} '
-            f'-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={FUZZ.LIB_DIR} '
-            f'-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY={FUZZ.LIB_DIR} '
-            f'{CMAKE_SOURCE_DIR} '
-        )
-
-        ctx.run('cmake --build . --target rrc_ue_test_helpers srsran_rrc -v')
+    targets.srsran.build_lib(
+        ctx=ctx,
+        cc=AFL.CLANG_FAST,
+        cxx=AFL.CLANG_FASTPP,
+        cflags='',
+        cxxflags='',
+        build_dir=FUZZ.BUILD_DIR,
+        lib_dir=FUZZ.LIB_DIR
+    )
 
 
 @task
 def build_coverage_lib(ctx: Context):
-    if COVERAGE.BUILD_DIR.exists() or COVERAGE.LIB_DIR.exists():
-        print('srsRAN coverage libraries already build...')
-        return
+    targets.srsran.build_lib(
+        ctx=ctx,
+        cc='clang',
+        cxx='clang++',
+        cflags='-fprofile-instr-generate -fcoverage-mapping',
+        cxxflags='-fprofile-instr-generate -fcoverage-mapping',
+        build_dir=COVERAGE.BUILD_DIR,
+        lib_dir=COVERAGE.LIB_DIR
+    )
 
-    ctx.run(f'mkdir {COVERAGE.BUILD_DIR}')
 
-    with ctx.cd(COVERAGE.BUILD_DIR):
+@task
+def build_bin(ctx: Context):
+    targets.srsran.build_bin(
+        ctx=ctx,
+        cc=AFL.CLANG_FAST,
+        cxx=AFL.CLANG_FASTPP,
+        cflags='-fsanitize=fuzzer',
+        cxxflags='-fsanitize=fuzzer',
+        lib_dir=FUZZ.LIB_DIR,
+        bin_dir=FUZZ.BIN_DIR,
+        bin=FUZZ.BIN
+    )
+
+
+@task
+def build_coverage_bin(ctx: Context):
+    targets.srsran.build_coverage_bin(
+        ctx=ctx,
+        cc='clang',
+        cxx='clang++',
+        cflags='-fprofile-instr-generate -fcoverage-mapping',
+        cxxflags='-fprofile-instr-generate -fcoverage-mapping',
+        lib_dir=COVERAGE.LIB_DIR,
+        bin_dir=COVERAGE.BIN_DIR,
+        bin=COVERAGE.BIN
+    )
+
+
+@task
+def run_fuzzer(ctx: Context):
+    ctx.run(f'mkdir -p {FUZZ.INFO_DIR}')
+
+    # Default AFL_SYNC_TIME is 30 minutes
+
+    ctx.run(
+        f'AFL_FINAL_SYNC=1 '  # still needs afl-cmin
+        f'{AFL.FUZZ} '
+        f'-i {AFL.INPUT_DIR} '
+        f'-o {AFL.OUTPUT_DIR} '
+        f'-s 42 '  # seed
+        f'-V 10 '  # fuzz for x seconds
+        f'-M fuzzer01 '  # main fuzzer
+        f'{FUZZ.BIN} '
+        f'> {FUZZ.INFO_DIR}/fuzz01.txt 2>&1 '
+        f'& '  # background process
+    )
+
+    num_cores = os.cpu_count()
+    free_cores = num_cores - 1
+
+    for i in range(free_cores):
+        core_id = i + 2  # start counting from 2
+        core_id_str = str(core_id).zfill(2)
+
         ctx.run(
-            f'cmake '
-            f'-G Ninja '
-            f'-DCMAKE_C_COMPILER=clang '
-            f'-DCMAKE_CXX_COMPILER=clang++ '
-            f'-DCMAKE_C_FLAGS="-fprofile-instr-generate -fcoverage-mapping" '
-            f'-DCMAKE_CXX_FLAGS="-fprofile-instr-generate -fcoverage-mapping" '
-            f'-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={COVERAGE.LIB_DIR} '
-            f'-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY={COVERAGE.LIB_DIR} '
-            f'{CMAKE_SOURCE_DIR} '
+            f'{AFL.FUZZ} '
+            f'-i {AFL.INPUT_DIR} '
+            f'-o {AFL.OUTPUT_DIR} '
+            f'-s 42 '  # seed
+            f'-V 10 '  # fuzz for x seconds
+            f'-S fuzzer{core_id_str} '  # secondary fuzzer
+            f'{FUZZ.BIN} '
+            f'> {FUZZ.INFO_DIR}/fuzz{core_id_str}.txt 2>&1 '
+            f'& '  # background process
         )
-
-        ctx.run('cmake --build . --target rrc_ue_test_helpers srsran_rrc -v')
-
-
-@task
-def build_harness(ctx: Context):
-    if FUZZ.BIN_DIR.exists():
-        print('harness already build...')
-        return
-
-    ctx.run(f'mkdir {FUZZ.BIN_DIR}')
-
-    ctx.run(
-        f'{AFL.CLANG_FASTPP} '
-        f'harness.cpp '
-        f'-fuse-ld=lld '
-        f'-I {CMAKE_SOURCE_DIR} '
-        f'-I {CMAKE_SOURCE_DIR}/include '
-        f'-I {CMAKE_SOURCE_DIR}/external '
-        f'-I {CMAKE_SOURCE_DIR}/external/fmt/include '
-        f'{FUZZ.LIB_DIR}/*.a '
-        f'-lgtest '
-        f'-lmbedtls -lmbedx509 -lmbedcrypto '
-        f'-o {FUZZ.BIN} '
-    )
-
-
-@task
-def build_coverage_harness(ctx: Context):
-    if COVERAGE.BIN_DIR.exists():
-        print('coverage harness already build...')
-        return
-
-    ctx.run(f'mkdir {COVERAGE.BIN_DIR}')
-
-    ctx.run(f'clang {STANDALONE_FUZZ_TARGET_MAIN}.c -c')
-
-    ctx.run(
-        f'clang++ '
-        f'{STANDALONE_FUZZ_TARGET_MAIN}.o '
-        f'harness.cpp '
-        f'-fprofile-instr-generate -fcoverage-mapping '
-        f'-fuse-ld=lld '
-        f'-I {CMAKE_SOURCE_DIR} '
-        f'-I {CMAKE_SOURCE_DIR}/include '
-        f'-I {CMAKE_SOURCE_DIR}/external '
-        f'-I {CMAKE_SOURCE_DIR}/external/fmt/include '
-        f'{COVERAGE.LIB_DIR}/*.a '
-        f'-lgtest '
-        f'-lmbedtls -lmbedx509 -lmbedcrypto '
-        f'-o {COVERAGE.BIN} '
-    )
 
 
 @task
@@ -172,7 +172,7 @@ def generate_coverage_info(ctx: Context):
     ctx.run(
         f'LLVM_PROFILE_FILE="{COVERAGE.LLVM_PROFRAW}" '
         f'{COVERAGE.BIN} '
-        f'{CORPUS_DIR}/* '
+        f'{AFL.CORPUS_DIR}/* '
     )
 
     ctx.run(
@@ -194,45 +194,6 @@ def generate_coverage_info(ctx: Context):
 
     with open(COVERAGE.TOTALS_JSON, 'w') as totals_file:
         json.dump(totals, totals_file, indent=4)
-
-
-@task
-def run_fuzzer(ctx: Context):
-    ctx.run(f'mkdir -p {FUZZ.INFO_DIR}')
-
-    # Default AFL_SYNC_TIME is 30 minutes
-
-    ctx.run(
-        f'AFL_FINAL_SYNC=1 '  # still needs afl-cmin
-        f'{AFL.FUZZ} '
-        f'-i {AFL.INPUT_DIR} '
-        f'-o {AFL.OUTPUT_DIR} '
-        f'-s 42 '  # seed
-        f'-V 10 '  # fuzz for x seconds
-        f'-M fuzzer01 '  # main fuzzer 
-        f'{FUZZ.BIN} '
-        f'> {FUZZ.INFO_DIR}/fuzz01.txt 2>&1 '
-        f'& '  # background process
-    )
-
-    num_cores = os.cpu_count()
-    free_cores = num_cores - 1
-
-    for i in range(free_cores):
-        core_id = i + 2  # start counting from 2
-        core_id_str = str(core_id).zfill(2)
-
-        ctx.run(
-            f'{AFL.FUZZ} '
-            f'-i {AFL.INPUT_DIR} '
-            f'-o {AFL.OUTPUT_DIR} '
-            f'-s 42 '  # seed
-            f'-V 10 '  # fuzz for x seconds
-            f'-S fuzzer{core_id_str} '  # secondary fuzzer 
-            f'{FUZZ.BIN} '
-            f'> {FUZZ.INFO_DIR}/fuzz{core_id_str}.txt 2>&1 '
-            f'& '  # background process
-        )
 
 
 def main():
---
+++ fuzz_benchmark/targets/srsran/__init__.py
@@ -0,0 +1,131 @@
+import pathlib
+
+from invoke import Context
+
+THIS_SCRIPT_DIR = pathlib.Path(__file__).parent.resolve()
+CMAKE_SOURCE_DIR = THIS_SCRIPT_DIR.parent.parent.parent.resolve()
+
+HARNESS_CPP = THIS_SCRIPT_DIR / 'harness.cpp'
+STANDALONE_FUZZ_TARGET_MAIN_C = THIS_SCRIPT_DIR / '../StandaloneFuzzTargetMain.c'
+STANDALONE_FUZZ_TARGET_MAIN_O = THIS_SCRIPT_DIR / '../StandaloneFuzzTargetMain.o'
+
+
+def build_lib(
+        ctx: Context,
+        cc: pathlib.Path,
+        cxx: pathlib.Path,
+        cflags: str,
+        cxxflags: str,
+        build_dir: pathlib.Path,
+        lib_dir: pathlib.Path,
+):
+    if build_dir.exists() or lib_dir.exists():
+        print('libraries already build...')
+        return
+
+    ctx.run(f'mkdir -p {build_dir}')
+
+    with ctx.cd(build_dir):
+        ctx.run(
+            f'cmake '
+            f'-G Ninja '
+            f'-DCMAKE_C_COMPILER="{cc}" '
+            f'-DCMAKE_CXX_COMPILER="{cxx}" '
+            f'-DCMAKE_C_FLAGS="{cflags}" '
+            f'-DCMAKE_CXX_FLAGS="{cxxflags}" '
+            f'-DCMAKE_LIBRARY_OUTPUT_DIRECTORY="{lib_dir}" '
+            f'-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY="{lib_dir}" '
+            f'{CMAKE_SOURCE_DIR} '
+        )
+
+        ctx.run('cmake --build . --target rrc_ue_test_helpers srsran_rrc -v')
+
+
+def internal_build_bin(
+        ctx: Context,
+        cc: pathlib.Path,
+        cxx: pathlib.Path,
+        cflags: str,
+        cxxflags: str,
+        lib_dir: pathlib.Path,
+        bin_dir: pathlib.Path,
+        bin: pathlib.Path,
+        optional_main: pathlib.Path | str
+):
+    if bin_dir.exists():
+        print('binary already build...')
+        return
+
+    ctx.run(f'mkdir -p {bin_dir}')
+
+    ctx.run(
+        f'{cxx} '
+        f'{optional_main} '
+        f'{HARNESS_CPP} '
+        f'{cxxflags} '
+        f'-fuse-ld=lld '
+        f'-I {CMAKE_SOURCE_DIR} '
+        f'-I {CMAKE_SOURCE_DIR}/include '
+        f'-I {CMAKE_SOURCE_DIR}/external '
+        f'-I {CMAKE_SOURCE_DIR}/external/fmt/include '
+        f'{lib_dir}/*.a '
+        f'-lgtest '
+        f'-lmbedtls -lmbedx509 -lmbedcrypto '
+        f'-o {bin} '
+    )
+
+
+def build_bin(
+        ctx: Context,
+        cc: pathlib.Path,
+        cxx: pathlib.Path,
+        cflags: str,
+        cxxflags: str,
+        lib_dir: pathlib.Path,
+        bin_dir: pathlib.Path,
+        bin: pathlib.Path
+):
+    internal_build_bin(
+        ctx=ctx,
+        cc=cc,
+        cxx=cxx,
+        cflags=cflags,
+        cxxflags=cxxflags,
+        lib_dir=lib_dir,
+        bin_dir=bin_dir,
+        bin=bin,
+        optional_main=''
+    )
+
+
+def build_coverage_bin(
+        ctx: Context,
+        cc: pathlib.Path,
+        cxx: pathlib.Path,
+        cflags: str,
+        cxxflags: str,
+        lib_dir: pathlib.Path,
+        bin_dir: pathlib.Path,
+        bin: pathlib.Path
+):
+    ctx.run(f'{cc} {STANDALONE_FUZZ_TARGET_MAIN_C} -c -o {STANDALONE_FUZZ_TARGET_MAIN_O}')
+
+    internal_build_bin(
+        ctx=ctx,
+        cc=cc,
+        cxx=cxx,
+        cflags=cflags,
+        cxxflags=cxxflags,
+        lib_dir=lib_dir,
+        bin_dir=bin_dir,
+        bin=bin,
+        optional_main=STANDALONE_FUZZ_TARGET_MAIN_O
+    )
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
